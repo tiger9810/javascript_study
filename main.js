f292fb67afb1c1549bf5df89eae9d9669b362988
Tiger98 <biguptiger@gmail.com>
2023-01-26 20:23:26 +0800

    good

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 'use strict';
-console.log("Hello World");
-console.log("Hello World from main.js");
-console.log(10 + 3);
-console.log(10 - 7);
-console.log(10 * 3);
-console.log(10 / 3);
-console.log(10 % 3);
-console.log(10 ** 3);
+//定数
+const price = 150;//代入
+
+console.log(price * 140);
