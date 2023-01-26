a4a9f2b31b661e4620a146d3ec8d3e5cfd732ff7
Tiger98 <biguptiger@gmail.com>
2023-01-27 07:42:49 +0800

    変数

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 'use strict';
-//定数
-const price = 150;//代入
+//定数 const←こちらを基本的に使う
+//変数 let←極力使わない
+//英数字, $, _のみで、数値から始められない。
+//大文字小文字は区別される。
+//予約語はつけられない。
+let price = 150;//代入
+
+console.log(price * 140);
+
+price = 170;
 
 console.log(price * 140);
