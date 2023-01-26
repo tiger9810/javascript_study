07f4bb9867623af9798ac1b3dac24e58720f7c62
Tiger98 <biguptiger@gmail.com>
2023-01-26 20:04:42 +0800

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 'use strict';
 console.log("Hello World");
 console.log("Hello World from main.js");
+console.log(10 + 3);
+console.log(10 - 7);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
