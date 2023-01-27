93d41ea0dd0a31c13317940bf652f626da7c33c3
Tiger98 <biguptiger@gmail.com>
2023-01-27 11:46:38 +0800

    配列の要素を操作する

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 'use strict';
 
-{
-    const scores = [80, 89, 90];
-    console.log(scores);
-    console.log(`scores: ${scores[0]}`)
-    console.log(`scores: ${scores[1]}`)
-    console.log(`scores: ${scores[2]}`)
-    console.log(scores.length);
+//配列の操作
+const array = [4, 5, 8];
+console.log(array);
+//先頭に要素を追加する場合→ unshift()
+array.unshift(4);
+console.log(array);
 
-    //ループ処理を使うと短く書ける
-    for (let i = 0; i < 3; i++) {
-        console.log(`scores: ${scores[i]}`)
-    }
-    console.log(scores.length);
-    //要素数もlengthで取得しておけば、変更があっても要素の数だけforで処理できる。
-    for (let i = 0; i < scores.length; i++) {
-        console.log(`scores: ${scores[i]}`)
-    }
-}
-//変数名.lengthで要素数を取得できる
+//先頭の要素を削除する場合→ shift()
+array.shift()
+console.log(array);
+
+//末尾に要素を追加する場合→ push()
+array.push(7, 8, 9)
+console.log(array);
+
+//末尾の要素を削除する場合→ pop()
+array.pop()
+console.log(array);
