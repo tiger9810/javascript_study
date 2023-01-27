4dd8cc721a7e35f24cc2ec06cc1a90c6dd281f96
Tiger98 <biguptiger@gmail.com>
2023-01-27 08:49:43 +0800

    if elseの省略記法→条件演算子

--- main.js
+++ main.js
@@ -5,26 +5,17 @@
 // } else {
 //     falseだった時の処理;
 // }
-
-// if (条件式A) {
-//     条件式Aがtrueだった時の処理;
-
-// } else if (条件式B){
-//     AがfalseでBがtrueだった時の処理;
-
-// } else {
-//     AもBもfalseだった時の処理
-// }
-
-//80点以上→Great,60点以上→Good,60点以下→Umm,と表示する
-//{}中かっこで囲われている部分をブロックという
 const score = 59;
 
 if (score >= 80) {
     console.log('Great');
 
-} else if (score >= 60) {
-    console.log('Good');
 } else {
     console.log('Umm...');
 }
+
+// 条件演算子
+// 条件式 ? trueの処理 : falseの処理
+const value = 50;
+value >= 80 ? console.log("great") : console.log('Umm...');
+
