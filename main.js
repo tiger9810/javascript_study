ab8543260b7b3cd1c3a95be23038d771b2445499
Tiger98 <biguptiger@gmail.com>
2023-01-27 09:40:30 +0800

    ループ処理とテンプレートリテラル

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
 'use strict';
-
-const signal = 'yellow';
-
-// if (signal === 'red') {
-//     console.log('Stop!');
-// } else if (signal === 'yellow') {
-//     console.log('Caution!');
-// } else if (signal === 'blue'){
-//     console.log('Go!');
-// }
-//条件分岐で完全一致を多用する場合は、switch()という命令文を使用すると簡潔に書ける
-
-switch (signal) {
-    case 'red':
-        console.log('stop');
-        break;
-    case 'yellow':
-        console.log('caution');
-        break;
-    case 'blue':
-        console.log('go');
-        break;
-    default:
-        console.log("Wrong signal");
-        break;
+//for文とテンプレートリテラルで値を埋め込んで文字列とする
+for (let i = 1; i <= 10; i++) {
+    console.log(`hello ${i}`);
 }
