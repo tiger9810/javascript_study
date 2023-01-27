6160b72880025146dbc2d3708dfe65913289235a
Tiger98 <biguptiger@gmail.com>
2023-01-27 13:44:18 +0800

    forEach();を配列に使う

--- main.js
+++ main.js
@@ -5,34 +5,11 @@ const array = [4, 5, 8];
 console.log(array);
 
 //index番号2から4つ要素を追加する場合
-array.splice(2, 0, 45, 32, 22, 1);
+array.splice(2, 0, 45, 32);
 console.log(array);
 
-//分割代入
-//配列の個々の要素を定数に代入する。
-const [a, b, c, d, e, f] = array;
+//forEach((引数)関数)要素の数だけ処理を実行してくれる
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
-
-//要素の途中までしか定数に追加しないとき...のレスト構文を使う
-//...新しい配列名でその配列に残りの要素を代入する。
-const [g, h, ...remain] = array;
-console.log(g);
-console.log(h);
-console.log(remain);
-
-//分割代入は値の交換にもよく使われる
-let x = 30;
-let y = 20;
-
-console.log(x);
-console.log(y);
-[x, y] = [y, x];
-
-console.log(x);
-console.log(y);
+array.forEach((score, index) => {
+    console.log(`score: ${score}: ${index}`);
+});
