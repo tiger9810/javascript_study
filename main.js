a9cf421e313286b5de37cfbe5e08be6d2a1366a4
Tiger98 <biguptiger@gmail.com>
2023-01-27 09:45:02 +0800

    do whileループ

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 //for文とテンプレートリテラルで値を埋め込んで文字列とする
-for (let i = 1; i <= 10; i++) {
-    console.log(`hello ${i}`);
-}
+
+let hp = 100;
+
+do {
+    console.log(`${hp} HP `);
+    hp -= 15;
+} while (hp > 0);
