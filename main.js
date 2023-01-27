3e091da63d1f186e65146c29c3932e62b157bc7b
Tiger98 <biguptiger@gmail.com>
2023-01-27 11:52:02 +0800

    splice構文で配列の要素を操作

--- main.js
+++ main.js
@@ -18,3 +18,12 @@ console.log(array);
 //末尾の要素を削除する場合→ pop()
 array.pop()
 console.log(array);
+
+//途中の要素を操作する →配列名.splice(変化が開始する位置,　削除数, 追加する要素);
+//index番号2から1つ削除する場合
+array.splice(2, 1);
+console.log(array);
+
+//index番号2から4つ要素を追加する場合
+array.splice(2, 0, 45, 32, 22, 1);
+console.log(array);
