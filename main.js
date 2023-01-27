d095ce76edb81a53c5f78fc7560bbabc477fe0b4
Tiger98 <biguptiger@gmail.com>
2023-01-27 15:04:05 +0800

    filter関数

--- main.js
+++ main.js
@@ -8,13 +8,21 @@ console.log(array);
 array.splice(2, 0, 45, 32);
 console.log(array);
 
-//map関数で配列を作成する
-// const updateValues = array.map((value) => {
-//     return value + 20;
-// });
+//map関数は配列に何らかの処理をして新しい作成する
+//filter関数は配列の要素をチェックして、条件にあうものだけを抽出して別の配列として取得することができる
+//偶数の要素だけ抽出して新しい配列を作成する
+//引数一つの時は関数の引数のかっこ不要
+//filter(function) filterのfunctionの中の条件式でtrueになったものだけが抽出されて、新しい配列となる。
+const evenNumbers = array.filter(Number => {
+    if (Number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+});
 
-//省略記法
-//引数一つなのでかっこ省略
-//returnだけなので、{}省略
-const updateValues = array.map(value => value + 20);
-console.log(updateValues);
+//上記の条件式はtrueの時はtrueを返し、falseのときはfalseを返しているので、わざわざif文作成しなくてもよい
+//このように省略する
+const shortEvenNumbers = array.filter(Number => Number % 2 === 0);
+console.log(evenNumbers);
+console.log(shortEvenNumbers);
