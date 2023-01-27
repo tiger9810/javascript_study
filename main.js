8aa919a81dde842d61487d7b95a08541b02844b8
Tiger98 <biguptiger@gmail.com>
2023-01-27 08:28:34 +0800

    真偽値と比較演算子

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 'use strict';
 
-//15になる。文字列と数値でも計算できてしまう。
-console.log('5' * 3);
+const price = 1200;
 
-//2になる。String型でも数値として計算されてしまう。
-console.log('5' - '3');
+//より大きい
+console.log(price > 1000);//true
 
-//足し算の場合、数値と文字列の組み合わせは文字列として処理される。つまり、53になる
-console.log('5' + 3);
+//より小さい
+console.log(price < 1000);//false
 
-//parseInt('4', 10)とすると、文字列を10進数の整数に変換する
-console.log(parseInt('5', 10) + 3);
-//出力は53→8となる。
+//以上
+console.log(price >= 1000);//true
+
+//以下
+console.log(price <= 1000);//false
+
+//等しい
+console.log(price === 1000);//false
+
+//異なる
+console.log(price !== 1000);//true
+
+// false <- 0, null, undefined, '', false
+// true <- それ以外。
+
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean(5));
