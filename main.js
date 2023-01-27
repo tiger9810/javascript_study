16690e15644ee5612d2a602df2b9cc6b2fd58d21
Tiger98 <biguptiger@gmail.com>
2023-01-27 11:29:35 +0800

    commit to new branch!!

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
 'use strict';
-//関数宣言
-// function 関数名(仮引数1, 仮引数2, ...) {
-//     処理;
-//     return 返したい値;
-// }
 
-//関数式(定数、変数に関数を入れる、メリットはわからない)
-//定数、変数に代入された関数を無名関数という
-//関数式の際にはブロック{}の最後に;を必要とする
-// const 定数名 = function(仮引数1, 仮引数2, ...) {
-//     処理;
-//     return 返したい値(返り値という);
-// }; ←;必要！！
-
-//アロー関数を使用した省略記法ではfunctionを消して、=の後に直接引数を持ってくる
-
-const sum1 = function (a, b, c) {
-    return a + b + c;
+{
+    const scores = [80, 89, 90];
+    console.log(scores);
 }
-
-//アロー関数を使用した省略記法
-const sum2 = (a, b, c) =>  a + b + c;
-
-console.log(sum1(3, 4, 5));
-console.log(sum2(3, 4, 5));
-
-//引数が一つの場合は()も省略できる。
-const double1 = function(a) {
-    return a * 2;
-}
-
-console.log(double1(2));
-
-const double2 = a => a * 2;
-
-console.log(double2(3));
