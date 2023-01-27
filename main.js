9c32d0e928107064e768991e1238b4a0e24486aa
Tiger98 <biguptiger@gmail.com>
2023-01-27 09:38:07 +0800

    switch文

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 'use strict';
 
-const score = 80;
-const name = 'tani';
+const signal = 'yellow';
 
-//2つの条件を一行で表せる。
-if (score >= 80 && name === 'tani') {
-    console.log('good');
+// if (signal === 'red') {
+//     console.log('Stop!');
+// } else if (signal === 'yellow') {
+//     console.log('Caution!');
+// } else if (signal === 'blue'){
+//     console.log('Go!');
+// }
+//条件分岐で完全一致を多用する場合は、switch()という命令文を使用すると簡潔に書ける
+
+switch (signal) {
+    case 'red':
+        console.log('stop');
+        break;
+    case 'yellow':
+        console.log('caution');
+        break;
+    case 'blue':
+        console.log('go');
+        break;
+    default:
+        console.log("Wrong signal");
+        break;
 }
-
-// && || !の三種類
