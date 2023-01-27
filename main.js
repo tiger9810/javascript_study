9581ad2c84d95b542e8566273dc9228a8a604172
Tiger98 <biguptiger@gmail.com>
2023-01-27 11:40:44 +0800

    for文で配列の中身を表示する

--- main.js
+++ main.js
@@ -3,6 +3,19 @@
 {
     const scores = [80, 89, 90];
     console.log(scores);
+    console.log(`scores: ${scores[0]}`)
+    console.log(`scores: ${scores[1]}`)
+    console.log(`scores: ${scores[2]}`)
     console.log(scores.length);
+
+    //ループ処理を使うと短く書ける
+    for (let i = 0; i < 3; i++) {
+        console.log(`scores: ${scores[i]}`)
+    }
+    console.log(scores.length);
+    //要素数もlengthで取得しておけば、変更があっても要素の数だけforで処理できる。
+    for (let i = 0; i < scores.length; i++) {
+        console.log(`scores: ${scores[i]}`)
+    }
 }
-//変数名.lengthで変数名を取得できる
+//変数名.lengthで要素数を取得できる
