aad2985edc01c400df5d7bda2959aab3237fc91a
Tiger98 <biguptiger@gmail.com>
2023-01-27 13:39:56 +0800

    分割代入とレスト構文

--- main.js
+++ main.js
@@ -8,14 +8,31 @@ console.log(array);
 array.splice(2, 0, 45, 32, 22, 1);
 console.log(array);
 
-//スプレッド構文　配列の中に配列を展開する
-const array2 = [100, 101, 102];
+//分割代入
+//配列の個々の要素を定数に代入する。
+const [a, b, c, d, e, f] = array;
 
-const marge = [...array, ...array2];
-console.log(marge);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
 
-//スプレッド構文は配列の中身だけを展開するので、関数の引数によく使われる
+//要素の途中までしか定数に追加しないとき...のレスト構文を使う
+//...新しい配列名でその配列に残りの要素を代入する。
+const [g, h, ...remain] = array;
+console.log(g);
+console.log(h);
+console.log(remain);
 
-const sum = (a, b, c) => a + b + c;
+//分割代入は値の交換にもよく使われる
+let x = 30;
+let y = 20;
 
-console.log(sum(...array));
+console.log(x);
+console.log(y);
+[x, y] = [y, x];
+
+console.log(x);
+console.log(y);
