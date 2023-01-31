1e14005a3c3397aac25552af0e6f0ac6a0f715fd
Tiger98 <biguptiger@gmail.com>
2023-02-01 05:55:35 +0800

    タイマーとsetInterval()を使う

--- main.js
+++ main.js
@@ -2,39 +2,21 @@
 //日時に関する命令
 
 //現在時刻の取得　new Date();
-//自分の誕生日が何曜日かを知る
-//ツェラーの公式というものがあるらしい。
-const d = new Date(1998, 9, 9);
-//.setHours()で時間分秒をあとから指定できる。
-d.setHours(22, 25, 24);
-console.log(d);
+//現在日時をconsoleに表示する関数
+// const showTime = () => {
+//     return console.log(new Date());
+// }
+let i = 0;
+function showTime() {
+    console.log(new Date());
+    i++;
+}
 
-//警告 alert('文字列');
-alert('誕生日の曜日をを知りたくない方は閉じてください！！')
-//確認 confirm('文字列');
-const answer = confirm('自分の誕生日が何曜日かしりたいデスカ...？');
-
-if (answer) {
-    switch (d.getDay()) {
-        case 0: 
-            alert("Sunday!");
-            break;
-        case 1:
-            alert("Monday");
-            break;
-        case 2:
-            alert("Tuesday");
-            break;
-        case 3:
-            alert("Wednesday");
-            break;
-        case 4:
-            alert("Thursday");
-            break;
-        case 5:
-            alert("Friday");
-            break;
-        case 6:
-            alert("Saturday");
-            break;
-}}
+//setInterval(1秒毎に表示したい関数, 時間をミリ秒で指定)を使用して1秒ごとに表示させる
+// setInterval((showTime), 1000);
+//関数を引数として渡す場合は、かっこをつけない
+if (i > 2){
+    //setIntervalを止める処理clearInterval(止めたい関数を格納した定数)
+    clearInterval(showTime);
+}
+const intervalId = setInterval(showTime, 1000);
