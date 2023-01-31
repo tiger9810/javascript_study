3d7cc36ace79d8a5d753bc5519c873fa41700c0d
Tiger98 <biguptiger@gmail.com>
2023-01-31 21:45:47 +0800

    警告や確認のダイアログ

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ const d = new Date(1998, 9, 9);
 //.setHours()で時間分秒をあとから指定できる。
 d.setHours(22, 25, 24);
 console.log(d);
+
+//警告 alert('文字列');
+alert('誕生日の曜日をを知りたくない方は閉じてください！！')
+//確認 confirm('文字列');
+const answer = confirm('自分の誕生日が何曜日かしりたいデスカ...？');
+
+if (answer) {
+    switch (d.getDay()) {
+        case 0: 
+            alert("Sunday!");
+            break;
+        case 1:
+            alert("Monday");
+            break;
+        case 2:
+            alert("Tuesday");
+            break;
+        case 3:
+            alert("Wednesday");
+            break;
+        case 4:
+            alert("Thursday");
+            break;
+        case 5:
+            alert("Friday");
+            break;
+        case 6:
+            alert("Saturday");
+            break;
+}}
