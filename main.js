474c576dba92aa5a25b9392fb5df21ab48cc9be8
Tiger98 <biguptiger@gmail.com>
2023-02-02 10:25:03 +0800

    静的メソッドの仕組み

--- main.js
+++ main.js
@@ -2,20 +2,11 @@
 
 //SNSのいいねと投稿の要素を持つを持つobjectを作成する
 
-//postsのテンプレートをクラス、クラスから作ったオブジェクトをインスタンスという
-
-//class クラス名でクラスを作成できる
-//constructor()メソッドでクラスを初期化することによって、インスタンスのプロパティをセットする
-//クラスを定義するうえで、this.key名でインスタンスを作成した際にできるプロパティのKeyを設定する
-//constructor()メソッドの引数にはインスタンスごとに違う値をとるValueを設定する
 class Posts {
     constructor(text){
-        //オブジェクトはkey: value,で設定しているが、classでインスタンスのプロパティを定義するときはthis.key = value;で設定する
         this.text = text;
         this.likeCount = 0;
     }//セミコロンいらない
-    //メソッドはconstructor()の外で定義する
-
     show() {
         console.log(`${this.text}'s likecount: ${this.likeCount}`);
     }
@@ -23,18 +14,26 @@ class Posts {
         this.likeCount++;
         this.show();
     }
-
+//インスタンスを介さずにclassから直接メソッドを呼び出す方法
+//class自体の説明を表示するメソッドを作成 static
+//this.は使用できない
+//class内で使われるthisはインスタンス(クラスから作られたオブジェクト)なので、インスタンスを作成しない静的メソッドでは使用できない。
+    static showInfo() {
+        console.log('Post class version 1.0');
+    }
 }
-
-//インスタンスの作りかた
-//new Class名(引数)で作成できる。
 
 const posts2 = [
     new Posts('hello first post!'),
     new Posts('next post comming soon'),
 ];
 
+
+
 posts2[0].show();
 posts2[0].like();
 //posts2[0].likeCount++:ではなくて、like()メソッドを使用するのが推奨
 posts2[1].show();
+
+//static function()で設定した静的メソッドを直接呼び出す
+Posts.showInfo();
