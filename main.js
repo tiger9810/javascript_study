d83bf0f6850de30476ef44153736123c3192975d
Tiger98 <biguptiger@gmail.com>
2023-02-02 09:43:25 +0800

    何度も挫折したclass

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
 'use strict';
 
-//ユーザーから名前を受け取って、大文字にする処理.toUpperCase()は文字列しか受け取れない
-// const name = 'tiger98';
-const name = 98;
-//文字列以外だった場合の処理
-//例外が起きそうな個所をtry()で囲ってあげる
-try {
-    console.log(name.toUpperCase());
-} catch (e){
-    console.log(e)
+//SNSのいいねと投稿の要素を持つを持つobjectを作成する
+const posts = [
+    {
+        text: 'hello',
+        likeCount: 0,
+        //メソッドとして定義するときはfunctionいらない
+        show() {
+            //同じオブジェクト内のプロパティはthisで表現できる。ここではposts[0]=thisという感じ
+            console.log(`${this.text}のいいね数は${post.likeCount}です`);
+        }
+    },
+    {
+        text: '2nd post!',
+        likeCount: 1,
+        show() {
+            console.log(`${this.text}:likeCount${this.likeCount}`)
+        }
+    },
+];
+
+//オブジェクト内で定義するとメソッドと呼ばれる
+function show(post) {
+    console.log(`${post.text}のいいね数は${post.likeCount}です`);
 }
 
-//try{}catch{}で囲っていれば、finishまで表示できる
-console.log('finish!!');
-//catchで例外が起きた後の処理を{}に記述する
-//catch(e)、(e)を引数にとると、error情報を引数eに格納できる
+// show(posts[1]);
+//オブジェクト.関数で呼び出せる
+posts[1].show();
+
+//postsのテンプレートをクラス、クラスから作ったオブジェクトをインスタンスという
+
+//class クラス名でクラスを作成できる
+//constructor()メソッドでクラスを初期化することによって、インスタンスのプロパティをセットする
+//クラスを定義するうえで、this.key名でインスタンスを作成した際にできるプロパティのKeyを設定する
+//constructor()メソッドの引数にはインスタンスごとに違う値をとるValueを設定する
+class Posts {
+    constructor(text){
+        //オブジェクトはkey: value,で設定しているが、classでインスタンスのプロパティを定義するときはthis.key = value;で設定する
+        this.text = text;
+        this.likeCount = 0;
+    }//セミコロンいらない
+    //メソッドはconstructor()の外で定義する
+
+    show() {
+        console.log(`${this.text}'s likecount: ${this.likeCount}`);
+    }
+
+}
+
+//インスタンスの作りかた
+//new Class名(引数)で作成できる。
+
+const posts2 = [
+    new Posts('hello first post!'),
+    new Posts('next post comming soon'),
+];
+
+posts2[0].show();
