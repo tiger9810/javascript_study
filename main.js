7a77349a280dd1c92a4f5b880e6d808e3ce05dab
Tiger98 <biguptiger@gmail.com>
2023-02-03 10:49:12 +0800

    getElementByIdによるtitle属性の操作

--- main.js
+++ main.js
@@ -6,5 +6,9 @@
 //buttonの要素を取得する
 //clickされた時の動作を決める！！~addEventListener(イベントの種類を文字列で指定, 実行したい処理を関数で渡す)~
 document.querySelector('button').addEventListener('click', () => {
-    document.querySelector('p').textContent = 'first change!!';
+    document.getElementById('target').textContent = 'first change!!';
+
+    const targetNode = document.getElementById('target');
+    targetNode.textContent = 'Content was Changed by getElementById';
+    targetNode.title = 'title was Changed by getElementById';
 });
