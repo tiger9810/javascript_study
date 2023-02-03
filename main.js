b00ea07cf5a426c1fd9fe9f754860a5174c6a47d
Tiger98 <biguptiger@gmail.com>
2023-02-03 09:56:06 +0800

    idの操作

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 'use strict';
 
 //　DOM操作の基本
-//　HTMLを読み込むとDomというデータ構造ができる
-// そのデータをもとにページが描画される
-//　javascriptが操作してるのはDom
-//　データ構造自体を操作している
-//　HTML自体を操作しているわけではない
-//　よってHTMLファイルは書き換えられない
-//　HTMLのそれぞれのデータはNodeと呼ばれる
-//　documentから始まり、Domツリーと呼ばれる
-//　空白や字下げも一つのNodeとして扱われる
-//　HTMLの要素を表すNode要素はElementノード(html, body, titleなど)
-//　文字列、空白、改行の要素はTextノード
+//　Domはdocumentというオブジェクトで扱える
+//　documentオブジェクトのdocument.querySelector(操作したい要素を指定)というメソッドでHTML要素を操作できる
+// 
+function domOperation() {
+    document.querySelector('h1').textContent = 'Changed';//textContentでタグで囲われたコンテンツを指定できる。タグ→コンテンツの順に指定していく
+
+    //ある一つの要素(タグ＋コンテンツ)を操作するときにid属性を使用することもある
+    //ハッシュタグ＋id名で指定できる
+    document.querySelector('#target').textContent = "I'm beginner";
+    //documentオブジェクトにgetElementByIdメソッドがあってそれで指定もできる
+    document.getElementById('target2').textContent = "I'm practice now !";
+}
+
+
+
+//setTimeout(関数, 開始時刻);
+setTimeout(domOperation, 3000);
