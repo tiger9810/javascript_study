113a2a77e24e28a62330b99664b3d12863f1d4aa
Tiger98 <biguptiger@gmail.com>
2023-02-03 10:20:58 +0800

    querySelectorAllとforEach()の組み合わせ

--- main.js
+++ main.js
@@ -3,18 +3,24 @@
 //　DOM操作の基本
 //　Domはdocumentというオブジェクトで扱える
 //　documentオブジェクトのdocument.querySelector(操作したい要素を指定)というメソッドでHTML要素を操作できる
-// 
+// id属性がなければ、タグそのものを指定する。querySelectorAllで取得した要素は配列なので、forEach()を使用できる
 function domOperation() {
-    document.querySelector('h1').textContent = 'Changed';//textContentでタグで囲われたコンテンツを指定できる。タグ→コンテンツの順に指定していく
-
-    //ある一つの要素(タグ＋コンテンツ)を操作するときにid属性を使用することもある
-    //ハッシュタグ＋id名で指定できる
-    document.querySelector('#target').textContent = "I'm beginner";
-    //documentオブジェクトにgetElementByIdメソッドがあってそれで指定もできる
-    document.getElementById('target2').textContent = "I'm practice now !";
+    //tag → Contentの順番で指定する
+    //querySelectorでは一つ目のpタグしか操作できない。
+    document.querySelector('p').textContent = 'first change!!';
 }
+function changeP() {
+    document.querySelectorAll('p')[2].textContent = 'change element';
+}
+function changeAllp() {
+    document.querySelectorAll('p').forEach((p, index) => {
+        p.textContent = `${index}番目のpを変更しました。`
+    })
+} 
 
 
 
 //setTimeout(関数, 開始時刻);
-setTimeout(domOperation, 3000);
+setTimeout(domOperation, 1000);
+setTimeout(changeP, 2000);//←一つ目のsetTimeoutが終わるタイミングで開始するのではなく、同時刻に代わる
+setTimeout(changeAllp, 3000);
