fb36ddd2227c9dedc92981583ddc7d2f44e3b7d6
Tiger98 <biguptiger@gmail.com>
2023-02-03 14:55:33 +0800

    input要素でちょっとしたTodolistを作成する

--- main.js
+++ main.js
@@ -6,17 +6,13 @@
 //buttonの要素を取得する
 //clickされた時の動作を決める！！~addEventListener(イベントの種類を文字列で指定, 実行したい処理を関数で渡す)~
 document.querySelector('button').addEventListener('click', () => {
-    const targetNode = document.getElementById('target');
-    targetNode.textContent = 'Content was Changed by getElementById';
-    targetNode.title = 'title was Changed by getElementById';
-//リストの要素を追加する
-//まずli要素を追加する。document.createElement('要素名')
-    const item2 = document.createElement('li');
-//中身のテキストを設定する
-    item2.textContent = 'hello world again!!!!!'
-//DOMツリーに追加する（親要素に対して子要素となるものの末尾に追加する）
-    //親要素であるul要素を取得する。要素を取得できるquerySelectorを使う
-    const parentUl = document.querySelector('ul');
-    //append=付けるという意味の.appendChildで親要素に子要素を追加する。
-    parentUl.appendChild(item2);
+    const todo = document.createElement('li');
+    const text = document.querySelector('input');
+    //inputに入力された値はvalue属性で取得できる。
+    todo.textContent = text.value;
+    //appendChild(追加したい要素←ここではtodo)
+    document.querySelector('ul').appendChild(todo);
+    //textの中身をからにする
+    text.value = '';
+    text.focus();
 });
