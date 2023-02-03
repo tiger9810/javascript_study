2aef0799413a017e5d3d21143900bc25c9a01d73
Tiger98 <biguptiger@gmail.com>
2023-02-03 17:20:29 +0800

    form要素内のボタンをクリックすると発生するイベントsubmit

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
 'use strict';
 
-const text = document.querySelector('textarea');
-//テキストエリアにマウスフォーカスすると発生するイベント
-text.addEventListener('focus', () => {
-    console.log('focus');
-});
-
-//テキストエリア以外にマウスクリックすると発生するイベント
-text.addEventListener('blur', () => {
-    console.log('blur');
-});
-
-//テキストエリアに文字を入力したときに発生するイベント
-text.addEventListener('input', () => {
-    console.log('input');
-    console.log(text.value.length);
-});
-
-
-text.addEventListener('change', () => {
-    console.log('change');
-});
-
+//form内のボタンがクリックされた時のイベントは、submit.クエリーセレクターでもformを指定する
+//フォームが送信されたらページがリロードされちゃうので、console.logとか一瞬で消えてしまう。
+//formタグを指定して、submitイベントだとエンターキーで送信できる。
+document.querySelector('form').addEventListener('submit', (e) => {
+    //ページ遷移をキャンセルする
+    e.preventDefault();
+    console.log('hello');
+})
