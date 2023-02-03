3013feee254b15fca83dc0839efff0ac4808ebfb
Tiger98 <biguptiger@gmail.com>
2023-02-03 11:26:33 +0800

    data-から始まるカスタムデータ属性

--- main.js
+++ main.js
@@ -6,15 +6,12 @@
 //buttonの要素を取得する
 //clickされた時の動作を決める！！~addEventListener(イベントの種類を文字列で指定, 実行したい処理を関数で渡す)~
 document.querySelector('button').addEventListener('click', () => {
-    document.getElementById('target').textContent = 'first change!!';
-
     const targetNode = document.getElementById('target');
     targetNode.textContent = 'Content was Changed by getElementById';
     targetNode.title = 'title was Changed by getElementById';
-
-    //既存のクラスにclassList.add('追加したいクラス名')でクラスを追加できる
-    // targetNode.classList.add('my-color');
-
-    //クラスを追加したり、削除したりできるtoggle
-    targetNode.classList.toggle('my-color');
+//クリックすると和訳が表示されるようにする
+    // targetNode.textContent = 'こんにちは　世界！'
+    //target属性を指定するのではなく、data-から始まるカスタムデータ属性を指定してみる
+    //指定したカスタムデータ属性に代入している文字列がid=targetのp要素に代入される
+    targetNode.textContent = targetNode.dataset.translation;
 });
